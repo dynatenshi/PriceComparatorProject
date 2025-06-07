635c49c479eb8c4fbb0c6b9b24fefc31359efab6
dynatenshi <lomonosov.fedechka@gmail.com>
2025-06-07 16:44:07 +0300

    Uploaded

---
+++ scraping.js
@@ -0,0 +1,31 @@
+let urls = ['https://zheleza.net/', 'https://www.regard.ru', 'https://www.netlab.ru'];
+let htmlData = [];
+let options = {
+    /*mode: 'no-cors',
+    credentials: 'include',*/
+    headers: {
+        'Access-Control-Allow-Origin': 
+    }
+}
+
+async function FetchDataLoop( targets = [] ) {
+    for (url of targets) {
+        //console.log(url);
+        try {
+            let promise = await fetch(url, options); 
+            if (!promise.ok) {
+                throw new Error("HTTP error! status: " + promise.status);
+            }
+            let data = await promise.text();
+            htmlData.push(data);
+            console.log("url: " + url + " l: " + htmlData.length);
+        }
+        catch(error) {
+            console.error("Error fetching data for item: ", url, error);
+        }
+    }
+
+    console.log("final l: " + htmlData.length);
+};
+
+FetchDataLoop(urls);
